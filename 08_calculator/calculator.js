401e85be5b6e2b37f03a0f7d3d9fe690d177c72a
Stephane Potey <poteystephane@gmail.com>
2023-07-13 07:25:54 -0400

    Answers Exercises

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,33 @@
-const add = function() {
-	
+const add = function (num1, num2) {
+  return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function (num1, num2) {
+  return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function (array) {
+  return array.reduce((total, current) => total + current, 0);
 };
 
-const multiply = function() {
-
+const multiply = function (...array) {
+  let multiplyBy = 1;
+  for (let i = 0; i < array.length; i++) {
+    multiplyBy *= array[i];
+  }
+  return multiplyBy;
 };
 
-const power = function() {
-	
-};
+const power = function () {};
 
-const factorial = function() {
-	
+const factorial = function (n) {
+  if (n === 0) return 1;
+
+  let product = 1;
+  for (let i = n; i > 0; i++) {
+    product *= i;
+  }
+  return product;
 };
 
 // Do not edit below this line
@@ -29,5 +37,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+  let arrayStr = str.split("").reverse().join("");
+  if (arrayStr.toLowerCase === str.toLowerCase) {
+    return true;
+  } else {
+    false;
+  }
 };
 
 // Do not edit below this line
